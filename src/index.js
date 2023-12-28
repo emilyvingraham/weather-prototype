fec0b12c8132e1356e9c754c05acb9c8dc641593
Emily Ingraham <emilyvingraham@gmail.com>
2023-12-28 16:54:52 -0700

    Injected date and time data from API

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ function updateData(response) {
   );
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector('#search-results-city');
-  let descriptionElement = document.querySelector("#description");
-  let longitudeElement = document.querySelector("#longitude");
+  let descriptionElement = document.querySelector('#description');
+  let longitudeElement = document.querySelector('#longitude');
   let latitudeElement = document.querySelector('#latitude');
   let countryElement = document.querySelector('#country');
   let feelsLikeElement = document.querySelector('#feels-like');
-  let humidityElement = document.querySelector("#humidity");
-  let windspeedElement = document.querySelector("#windspeed");
-  
-
+  let humidityElement = document.querySelector('#humidity');
+  let windspeedElement = document.querySelector('#windspeed');
+  let dateTimeElement = document.querySelector('#date-and-time');
+  let date = new Date(response.data.time * 1000);
 
   temperatureElement.innerHTML = `${temperature}`;
   cityElement.innerHTML = response.data.city;
@@ -22,10 +22,27 @@ function updateData(response) {
   countryElement.innerHTML = response.data.country;
   feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like);
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windspeedElement.innerHTML = `${response.data.wind.speed}km/h`
+  windspeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  dateTimeElement.innerHTML = formatDate(date);
+}
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  } else {
+    minutes;
+  }
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  } else {
+    hours;
+  }
+  let days = ['Sun.', 'Mon.', 'Tues.', 'Wed.', 'Thurs.', 'Fri.', 'Sat.'];
+  let day = days[date.getDay()];
 
-  console.log(response.data);
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
